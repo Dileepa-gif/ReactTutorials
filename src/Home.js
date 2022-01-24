9f04fffa8b355e7bf900e6fec37538f097ae5b36
Dileepa-gif <59864755+Dileepa-gif@users.noreply.github.com>
2022-01-24 15:02:46 +0530

    tutorial_7

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>HomePage</h2>
-        </div>
-     );
+
+  const handleClick = (e) => {
+    console.log('hello world', e);
+  }
+
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={(e) => handleClickAgain('Dileepa', e)}>Click again</button>
+    </div>
+  );
 }
  
 export default Home;
